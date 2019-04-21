0d48a3998045d0b349654e409018312444c7c619
Szymon <szymonwarszawa@gmail.com>
2019-04-21 15:34:32 +0100

    add Statistics class calculating number of games, wins and losses

---
+++ Statistics.js
@@ -0,0 +1,29 @@
+class Statistics {
+  constructor() {
+    this.gameResults = [{
+      win: true,
+      bid: 2
+    }, {
+      win: false,
+      bid: 1
+    }];
+  }
+  addGameToStatistics(win, bid) {
+    let gameResult = {
+      win: win,
+      bid: bid
+    }
+    console.log(gameResult)
+    this.gameResults.push(gameResult);
+  }
+
+  showGameStatistics() {
+    let games = this.gameResults.length;
+    let wins = this.gameResults.filter(result => result.win).length;
+    let losses = this.gameResults.filter(result => !result.win).length;
+    console.log(wins, losses);
+    return [games, wins, losses]
+  }
+}
+
+const stats = new Statistics()
